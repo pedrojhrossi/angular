d2fd650155e7a5a1a580aaa11c288c3a375db655
pedrojhrossi <pedrojhrossi@gmail.com>
2021-02-13 21:44:14 -0400

    Modificacion de la clase service para el consume del api Rest con Spring

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
-import { Observable, of } from 'rxjs'
-
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:8090/api/clientes';
+  constructor(private http: HttpClient) { }
 
-  getClientes(): Observable <Cliente[]> {
-    return of(CLIENTES);
+  getClientes(): Observable<Cliente[]> {
+    // return of(CLIENTES);
+    // return this.http.get<Cliente[]>(this.urlEndPoint);
+    //TODO: La siguiente forma retorna el listado de JSON en Cliente[], es lo mismo a 'this.http.get<Cliente[]>(this.urlEndPoint);'
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );
   }
 }
