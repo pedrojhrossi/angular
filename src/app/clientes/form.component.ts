b05e6042cc34a4c04c1575230dba287515548233
pedrojhrossi <pedrojhrossi@gmail.com>
2021-02-15 18:09:05 -0400

    Cliente Form created. 'form.component'

---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent implements OnInit {
+
+  private titulo: string = "Crear Cliente";
+  private cliente: Cliente = new Cliente();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public create(): void {
+    console.log("Clicked!");
+    console.log(this.cliente);
+  }
+
+}
